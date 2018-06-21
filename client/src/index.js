cf9f09f42404f504f02572c8ebd345fc091b6d07
Haowen <haowen737@gmail.com>
2018-06-21 21:54:20 +0800

    import router setting

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, Link } from 'react-router-dom'
+import createBrowserHistory from 'history/createBrowserHistory'
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './routes/home'
+import FancyPick from './routes/fancyPick'
+
+const history = createBrowserHistory()
+
+ReactDOM.render((
+  <Router history={history}>
+    <React.Fragment>
+      <Route exact path="/"component={App} />
+      <Route exact path="/fancy-pick" component={FancyPick} />
+    </React.Fragment>
+  </Router>
+), document.getElementById('root'));
 registerServiceWorker();
---
+++ client/src/routes/fancyPick/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class FancyPick extends Component {
+  render() {
+    return (
+      <div>
+        fancc
+      </div>
+    )
+  }
+}
